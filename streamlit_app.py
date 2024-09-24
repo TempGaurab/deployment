84f36313c1e619ef5c08c1c3dfff8eef49940d2d
TempGaurab <gaurabusa@gmail.com>
2024-09-24 10:35:17 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -102,7 +102,6 @@ def buttons():
                 course_details = algorithm.final(st.session_state['selected_course'])
                 graphs = graph.generate_graph(course_details)
                 st.write(f"### You entered: {st.session_state['selected_course']}")
-                st.write(course_details)
                 st.image(graphs, caption="Course Prerequisites Graph")
             else:
                 st.write("Please enter a course name.")
