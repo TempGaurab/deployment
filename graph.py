d311a1622e6019cb876482090ccb9033ed26d968
TempGaurab <gaurabusa@gmail.com>
2024-09-24 10:39:42 -0400

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,7 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 from io import BytesIO
-from networkx.drawing.nx_agraph import graphviz_layout
 
 def generate_graph(data):
     # Initialize a directed graph
@@ -13,9 +12,8 @@ def generate_graph(data):
             G.add_edge(prereq, course)
 
     # Draw the graph
-    nx.nx_agraph.write_dot(G,'test.dot')
     plt.figure(figsize=(10, 8))
-    pos=graphviz_layout(G, prog='dot')
+    pos = nx.spring_layout(G)  # position layout for nodes
     nx.draw(G, pos, with_labels=True, node_size=2000, node_color='lightblue', font_size=10, font_weight='bold', arrowsize=20)
     plt.title("Course Prerequisites Graph")
 
