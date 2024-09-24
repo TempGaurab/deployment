e187ed79c1f8d178ab8f25d2727fdb985df1ba27
TempGaurab <gaurabusa@gmail.com>
2024-09-24 15:32:50 -0400

    Update graph.py

--- graph.py
+++ graph.py
@@ -21,7 +21,7 @@ def generate_graph(data):
 
     # Draw the graph
     plt.figure(figsize=(10, 8))
-    node_colors = ['orange' if node == main_node else 'yellow' if node in G.predecessors(main_node) else 'skyblue' for node in G.nodes]
+    node_colors = ['orange' if node == main_node else '#ffa500' if node in G.predecessors(main_node) else '#add8e6' for node in G.nodes]
 
     # Generate the multipartite layout with the top-to-bottom orientation
     pos = nx.multipartite_layout(G, subset_key='subset', align='horizontal')
