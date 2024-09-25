dbb0254afef0f4da08811c43c504d795098df9bf
TempGaurab <gaurabusa@gmail.com>
2024-09-25 14:23:10 -0400

    Update graph.py

--- graph.py
+++ graph.py
@@ -33,9 +33,9 @@ def generate_graph(data):
     # Define node colors
     node_colors = [
         'orange' if node == main_node else 
-        '#E4B64D' if node in next_badge else 
-        '#C8C799' if node in second_badge else 
-        '#BFCDB3' if node in third_badge else 
+        '#E9B33F' if node in next_badge else 
+        '#E1B852' if node in second_badge else 
+        '#C3CAA4' if node in third_badge else 
         '#add8e6' 
         for node in G.nodes
     ]
